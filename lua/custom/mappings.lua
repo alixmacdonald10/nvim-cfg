1706ed4aa68124790c5942e790067e9616983ef1
Alix Macdonald <alixmacdonald10@googlemail.com>
2024-01-16 15:11:03 +0000

    added custom cfg

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
+
+M.crate = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    }
+  }
+}
+
+
+return M
